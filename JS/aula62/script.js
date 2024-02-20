6aaa859a9e5c2b57f59405a64aacb5427251d83a
Gabriel Centeno <52381051+100teno@users.noreply.github.com>
2024-02-20 10:52:49 -0300

    concat, arrays, rest and splice method

---
+++ JS/aula62/script.js
@@ -0,0 +1,12 @@
+// Valor por referencia
+//
+//              0           1           2           3           4
+// const nomes = ['Gabriel', 'Maiara', 'Henrique', 'Jonatan', 'Olidriana'];
+// const novo = nomes.slice(1, -2);
+// console.log(novo);
+
+const nomes = ['Gabriel', 'Oliveira', 'Centeno'];
+const nome = nomes.join('  ');
+console.log(nome);
+
+
---
+++ JS/aula63/script.js
@@ -0,0 +1,12 @@
+//              0            1          2           3           4
+const nomes = ['Gabriel', 'Maiara', 'Henrique', 'Olidriana', 'Jonatan'];
+//push
+nomes.splice(nomes.length, 0, 'Manuela');
+console.log(nomes);
+
+// const removidos = nomes.splice(4, 1);
+// pop
+// console.log(nomes, removidos);
+
+//shift
+// const removidos = nomes.splice(0, 1);
